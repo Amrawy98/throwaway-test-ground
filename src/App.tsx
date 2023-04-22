f9d9f95c48aa802c87993fbbbd53cb6fd90019a0
Ahmed Amrawy <ahmed.m.amrawy@gmail.com>
2023-04-22 19:01:18 +0200

    feat: create app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import { useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect } from "react";
+import { Outlet, matchPath, useLocation, useNavigate } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App">hello</div>;
+  const { isAuthenticated } = useAuth0();
+  const currentPath = useLocation().pathname;
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (isAuthenticated && !matchPath("/checkout", currentPath)) {
+      navigate("/checkout");
+    }
+    if (!isAuthenticated && !matchPath("/", currentPath)) {
+      navigate("/");
+    }
+  }, [isAuthenticated]);
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
