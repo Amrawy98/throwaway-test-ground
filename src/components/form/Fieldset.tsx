6363442041c8a486254df6c7444c5bd7eabdd7b7
Ahmed Amrawy <ahmed.m.amrawy@gmail.com>
2023-04-25 10:08:38 +0200

    feat: create form's fieldset

---
+++ src/components/form/Fieldset.tsx
@@ -0,0 +1,30 @@
+export type FieldsetProps = {
+  legend: string;
+  options: string[];
+  type: "checkbox" | "radio";
+};
+
+const Fieldset = ({ legend, options, type }: FieldsetProps) => {
+  return (
+    <fieldset className="block">
+      <legend className="text-gray-700">{legend}</legend>
+      <div className="mt-2">
+        {options.map((option, index) => (
+          <div>
+            <label className="inline-flex items-center">
+              <input
+                className={type === "checkbox" ? "form-checkbox" : "form-radio"}
+                type={type}
+                defaultChecked={index === 0}
+                defaultValue={index}
+              />
+              <span className="ml-2">{option}</span>
+            </label>
+          </div>
+        ))}
+      </div>
+    </fieldset>
+  );
+};
+
+export default Fieldset;
